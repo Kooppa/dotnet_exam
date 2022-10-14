fa63e34050da10cdb79ee3c8ee2052a2de3d9bb8
Humberto BM <89049194+Kooppa@users.noreply.github.com>
2022-10-14 13:51:38 -0500

    Services added

--- Controllers/CharacterController.cs
+++ Controllers/CharacterController.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
-
+using rpg_true.Services.CharacterService;
 
 namespace rpg_true.Controllers
 {
@@ -11,28 +11,33 @@ namespace rpg_true.Controllers
     [Route("api/[controller]")]
     public class CharacterController : ControllerBase
     {
-        private static List<Character> characters = new List<Character> {
-            new Character(),
-            new Character{ Id = 1, Name = "Killua"}
-        };
+        // private static List<Character> characters = new List<Character> {
+        //     new Character(),
+        //     new Character{ Id = 1, Name = "Killua"}
+        // };
+        private readonly ICharacterService _characterService;
+
+        public CharacterController(ICharacterService characterService)
+        {
+            _characterService = characterService;
+        }
 
         [HttpGet("GetAll")]
         public ActionResult<Character> Get()
         {
-            return Ok(characters);
+            return Ok(_characterService.GetAllCharacter());
         }
 
         [HttpGet("{id}")]
         public ActionResult<Character> GetSingle(int id)
         {
-            return Ok(characters.FirstOrDefault(c => c.Id == id));
+            return Ok(_characterService.GetCharacterById(id));
         }
 
         [HttpPost]
         public ActionResult<List<Character>> AddCharacter(Character newCharacter)
         {
-            characters.Add(newCharacter);
-            return Ok(characters);
+            return Ok(_characterService.AddCharacter(newCharacter));
         }
     }
 }
